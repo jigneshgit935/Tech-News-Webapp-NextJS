8d0d18912d26cdce2f901ecbb0b20981a42ae59e
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-08 15:18:06 +0530

    navbar and footer desinged and commited

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between pb-4 border-b mb-4">
+      <div className="">
+        <Link href="/">
+          <h1 className="text-4xl font-bold tracking-tighter text-dark">
+            Tech News
+          </h1>
+        </Link>
+        <p className="text-sm ">
+          Exploring Tommorrow&apos;s Innovations, <br /> One Byte at a Time.
+        </p>
+      </div>
+      <div className="flex items-center ">
+        <Link className="btn" href="/sign-in">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
+}
