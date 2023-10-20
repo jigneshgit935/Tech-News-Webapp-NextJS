06c6b8b7a45a273138c4cf72dbe167a03bd13e34
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-20 09:14:18 +0530

    apis created

---
+++ app/api/categories/route.ts
@@ -0,0 +1,13 @@
+import prisma from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const categories = await prisma.category.findMany()
+        return NextResponse.json(categories)
+
+    } catch (error) {
+        console.log("Error", error);
+        return NextResponse.json("Something went wrong")
+    }
+}
